0c0534dc10be907478cb2e8b53361123fb10fdbe
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-06-24 21:59:29 -0400

    set data to be stored locally and added alert with redirection and alert for uncompleted form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,9 @@
+const toggleModeBtn = document.getElementById("toggleMode");
+
+toggleModeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
 const form = document.getElementById("blogForm");
 
 form.addEventListener("submit", function (event) {
@@ -13,5 +19,16 @@ form.addEventListener("submit", function (event) {
       title: title,
       content: content,
     };
-}
-}
+
+    const existingPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
+
+    existingPosts.push(blogPost);
+
+    localStorage.setItem("blogPosts", JSON.stringify(existingPosts));
+
+    alert("Blog post submitted successfully! Redirecting to posts page.");
+    window.location.href = "blog.html";
+  } else {
+    alert("Please complete all fields in the form.");
+  }
+});
