75fa33cd8b6ad6b4dd58ca427313b73aa037fa6e
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-06-24 22:41:15 -0400

    programed collected data from the form to display on blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,6 +5,23 @@ toggleModeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const blogPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
+
+  const postsList = document.getElementById("postsList");
+
+  blogPosts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+            <h3>Title: ${post.title}</h3>
+            <p>${post.content}</p>
+            <p>Posted by: ${post.username}</p>
+        `;
+    postsList.appendChild(postElement);
+  });
+});
+
 backButton.addEventListener("click", function () {
   window.location.href = "index.html";
 });
